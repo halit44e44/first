0c9b3ec8c8ff3ad541ec7c474c2e8a3d28895d6c
halit44e44 <70605232+halit44e44@users.noreply.github.com>
2021-05-25 23:09:59 +0300

    frontend Soru işlemleri bitti

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Answer;
 use App\Models\Quiz;
+use App\Models\Result;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -31,21 +32,31 @@ class MainController extends Controller
 
     public function result(Request $request, $slug)
     {
-        $quiz = Quiz::whereSlug($slug)->with('questions')->withCount('questions')->first() ?? abort(404, "Sayfa bulunamadı");
-        $puan_dagilimi = 100 / $quiz->questions_count;
-        $total = 0;
+        $quiz = Quiz::whereSlug($slug)->with('questions')->first() ?? abort(404, "Sayfa bulunamadı");
+        $correct = 0;
+        
         foreach ($quiz->questions as $item) {
             Answer::create([
                 'user_id' => auth()->user()->id,
                 'question_id' => $item->id,
                 'answer' => $request->post($item->id)
             ]);
-            
             if ($item->correct_answer == $request->post($item->id)) {
-                $total += $puan_dagilimi;
+                $correct += 1;
             }
-            
         }
-        echo $total;
+        
+        $point = round((100 / count($quiz->questions)) * $correct);
+        $wrong = count($quiz->questions) - $correct;
+        Result::create([
+            'user_id' => auth()->user()->id,
+            'quiz_id' => $quiz->id,
+            'point' => $point,
+            'correct' => $correct,
+            'wrong' => $wrong
+
+        ]);
+
+        return redirect()->route('quiz.detail' , $quiz->slug)->withSuccess("Başarıyla Quizi Bitirdin Puanın: " . $point);
     }
 }
